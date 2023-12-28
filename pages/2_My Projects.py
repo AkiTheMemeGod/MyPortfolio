768ddb2e9326d74425d428b64f85aa2f2669bd53
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-28 18:46:35 +0530

    made cosmetic changes to my projects page and improved the ui and changed colors

---
+++ pages/2_My Projects.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(layout="wide")
+col1, empty_col,col2 = st.columns([1.5, 0.2, 1.5])
+data = pd.read_csv("data.csv", sep=";")
+with col1:
+    for index, row in data[:8].iterrows():
+        with st.container(border=1):
+            st.title(str(index + 1) + "." + row["title"])
+            st.subheader(f":rainbow[{str(row['progress'])}% Completed]")
+            st.progress(int(row['progress']))
+            st.subheader(row["description"])
+            st.info(f"Language : {row['language']}")
+
+            n1, n2, n3 = st.columns([0.25, 2, 0.25])
+            with n2:
+                st.image(f"images/{row['image']}", use_column_width="always")
+                st.markdown("###")
+                st.link_button(label="Source code",
+                               url=row['url'],
+                               use_container_width=True,
+                               type="primary")
+        st.markdown("###")
+with col2:
+    for index, row in data[8:].iterrows():
+        with st.container(border=1):
+            st.title(str(index + 1) + "." + row["title"])
+            st.subheader(f":rainbow[{str(row['progress'])}% Completed]")
+            st.progress(int(row['progress']))
+            st.subheader(row["description"])
+            st.info(f"Language : {row['language']}")
+
+            n1, n2, n3 = st.columns([0.25, 2, 0.25])
+            with n2:
+                st.image(f"images/{row['image']}", use_column_width="always")
+                st.markdown("###")
+                st.link_button(label="Source code",
+                               url=row['url'],
+                               use_container_width=True,
+                               type="primary")
+        st.markdown("###")
